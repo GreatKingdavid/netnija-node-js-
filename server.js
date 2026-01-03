645f26521c997e0f387665030e4a748c6a6cffe8
Okwor Kingdavid <okwordavid1@gmail.com>
2026-01-03 17:23:58 +0300

    we learnt about creating servers and how to call models and ude them in our server

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  let path = "./view/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader(`location`, "/about");
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  //set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // send file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listining for reqest on port 3000");
+});
