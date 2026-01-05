8388b429fa13d939fe3614ac275a09f48fc013af
Okwor Kingdavid <okingdavid5@gmail.com>
2026-01-05 03:07:33 +0300

    moving forward

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const express = require("express");
 //set up an express add
 const app = express();
 
+app.set('view engine', 'ejs')
+
 //listen for requests
-app.listen("3000");
+app.listen(3000);
 
 // responds to browser request
 app.get("/", (req, res) => {
-    res.send('<p> this is the home page </p>');
+  res.sendFile("./view/index.html", {root: __dirname});
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile("./view/about.html", {root: __dirname});
 });
+
+// redirect
+app.get("/about-me", (req, res)=>{
+    res.redirect('/about');
+})
+
+//404 page
+app.use((req,res)=>{
+    res.status(404).sendFile("./view/404.html", {root: __dirname});
+})
+
+
