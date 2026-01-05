e443bf542b161185eee435764784b6bda0099dac
Okwor Kingdavid <okingdavid5@gmail.com>
2026-01-06 01:46:44 +0300

    using ejs to display dynamic inputs

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const express = require("express");
 //set up an express add
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //listen for requests
 app.listen(3000);
 
 // responds to browser request
 app.get("/", (req, res) => {
-  res.render('index');
-})
-
-app.get("/about", (req, res) => {
-  res.render('about');
+  const blogs = [
+    {title: 'David is him', snippet: 'lpojksd poijdfsp onips wjndiop jopiwjdf opwndf'},
+    {title: 'David is him', snippet: 'lpojksd poijdfsp onips wjndiop jopiwjdf opwndf'},
+    {title: 'David is him', snippet: 'lpojksd poijdfsp onips wjndiop jopiwjdf opwndf'},
+  ]
+  res.render("index", { title: "home", blogs: blogs });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { title: "about" });
+});
 
-app.get("/blogs/create", (req, res)=>{
-    res.render('create');
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create" });
+});
 
 //404 page
-app.use((req,res)=>{
-    res.status(404).render('404');
-})
-
-
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
