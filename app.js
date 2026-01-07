fd0b4787bc56f83cd63671de4bb47f1bea37c4cf
Okwor Kingdavid <okingdavid5@gmail.com>
2026-01-07 21:02:28 +0300

    connected to database ManGoDB

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const express = require("express");
 //set up an express add
 const app = express();
 
+const dbURI = "mongodb+srv://major:code2002@nodejsblog.h1byyjb.mongodb.net/?appName=NodejsBlog"
+
 app.set("view engine", "ejs");
 
 //listen for requests
 app.listen(3000);
 
+//middelware and static files
+app.use(express.static('Public'))
+
 // responds to browser request
+app.use((req, res, next)=>{
+  console.log(`new request made`);
+  console.log(`host:`, req.hostname);
+  console.log(`path:`, req.path);
+  console.log(`method:`, req.method);
+  next()
+})
+
+
 app.get("/", (req, res) => {
   const blogs = [
     {title: 'David is him', snippet: 'lpojksd poijdfsp onips wjndiop jopiwjdf opwndf'},
